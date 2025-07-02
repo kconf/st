af4c66e19d4c2137a7806f2e7cb690f96ad12f10
Jianwei Han <hanjianwei@gmail.com>
2025-07-02 14:09:10 +0800

    chore(config): update font and installation prefix

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Maple Mono NF CN:Regular:size=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
